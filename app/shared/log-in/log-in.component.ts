31583eda52acdd43c351bf4fcf32c91ab71d9300
FIC <alandiaz@MacBook-Pro-de-Alan.local>
2026-05-10 21:46:43 -0600

    feat: ajustes finales tema RIFIC (header, login popup, mobile, i18n)

--- app/shared/log-in/log-in.component.ts
+++ app/shared/log-in/log-in.component.ts
@@ -3,10 +3,8 @@ import { LogInComponent as BaseComponent } from '../../../../../app/shared/log-i
 
 @Component({
   selector: 'ds-log-in',
-  // templateUrl: './log-in.component.html',
-  templateUrl: '../../../../../app/shared/log-in/log-in.component.html',
-  // styleUrls: ['./log-in.component.scss'],
-  styleUrls: ['../../../../../app/shared/log-in/log-in.component.scss'],
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.scss'],
 })
 export class LogInComponent extends BaseComponent {
 }
---
+++ app/shared/log-in/methods/password/log-in-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { LogInPasswordComponent as BaseComponent } from '../../../../../../../app/shared/log-in/methods/password/log-in-password.component';
+
+@Component({
+  selector: 'ds-log-in-password',
+  templateUrl: './log-in-password.component.html',
+  styleUrls: ['./log-in-password.component.scss'],
+})
+export class LogInPasswordComponent extends BaseComponent {
+}
